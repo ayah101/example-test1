6059fd02b901d19cc2c596eb95d0cbe9869dede6
ayah <ayjeren.nurjanova@sharecare.com>
2021-06-09 15:53:45 -0400

    2nd change

--- Find_Largest_NumLIst.py
+++ Find_Largest_NumLIst.py
@@ -1,7 +1,7 @@
 # FIND THE LARGEST NUMBER in the LIST
 
-names = ['Ayah','Bob','Jennifer','Christopher','London']
+names = ['Ayah', 'Bob', 'Jennifer', 'Christopher', 'London']
 print(names)
-
-
-
+print('ayah')
+print('lovely day')
+print('today is Wednesday')
